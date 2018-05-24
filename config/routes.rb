83a365b0cd5cb0d4c28bcd50484a48125867e66c
seimatz <contact@designeurasia.sakura.ne.jp>
2018-05-24 21:59:33 +0900

    redirect

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  get '' => 'games#top'
+  get '' => redirect_to("https://cyrillic.typing-up.pro/", status: 301)
+  # get '' => 'games#top'
   get 'games/level/:level' => 'games#index'
   get 'contents/setting' => 'contents#setting'
 end
